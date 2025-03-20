2bfea95fac5289815f14103c852ad81847ba6979
Oksana Dionisieva <atty.ltd@gmail.com>
2025-03-20 14:46:48 +0200

    Added views for data displaying

---
+++ BookApplication/Modules/Pages/Views/BookItemView.swift
@@ -0,0 +1,37 @@
+//
+//  BookItemView.swift
+//  BookApplication
+//
+//  Created by Oksana Dionisieva on 20.03.2025.
+//
+
+import SwiftUI
+
+struct BookItemView: View {
+    let item: BookItem
+    
+    var body: some View {
+        switch item.type {
+        case "text":
+            Text(item.title ?? "").font(.body)
+        case "image":
+            if let urlString = item.src, let url = URL(string: urlString) {
+                NavigationLink(destination: ImageDetailsView(imageURL: url, title: item.title ?? "")) {
+                    AsyncImage(url: url) { image in
+                        image.resizable().scaledToFit().frame(height: 100)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+            }
+        case "section":
+            Section(header: Text(item.title ?? "").font(.title2)) {
+                ForEach(item.items ?? []) { subItem in
+                    BookItemView(item: subItem)
+                }
+            }
+        default:
+            EmptyView()
+        }
+    }
+}
