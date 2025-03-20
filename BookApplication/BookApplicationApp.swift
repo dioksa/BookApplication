0873e5fa31ebfd35f8576c1e436e10f63cd416fe
Oksana Dionisieva <atty.ltd@gmail.com>
2025-03-20 11:33:46 +0200

    Reorder init folder

---
+++ BookApplication/BookApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookApplicationApp.swift
+//  BookApplication
+//
+//  Created by Oksana Dionisieva on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
